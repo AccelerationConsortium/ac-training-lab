414b6b8591630e45a92a4f561293a753bd894557
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-01-26 15:52:38 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
