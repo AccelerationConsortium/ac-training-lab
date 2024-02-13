9dfb61c829abb4a41faa07e0fc9b8356781f8e4f
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-02-13 10:25:48 -0500

    wrapping of table

--- docs/conf.py
+++ docs/conf.py
@@ -205,9 +205,15 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
