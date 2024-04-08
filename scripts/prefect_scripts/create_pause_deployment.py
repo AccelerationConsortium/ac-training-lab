5de9f833f55baced7585631f3aaa6430c6bca956
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 14:23:02 -0400

    change dir

--- scripts/prefect_scripts/create_pause_deployment.py
+++ scripts/prefect_scripts/create_pause_deployment.py
@@ -3,9 +3,9 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/AccelerationConsortium/ac-training-lab.git",
-        entrypoint="scripts/prefect/my_gh_workflow.py:repo_info",
+        entrypoint="scripts/prefect_scripts/my_gh_pause_workflow.py:greet_user",
     ).deploy(
-        name="my-first-deployment",
+        name="pause-workflow-deployment",
         work_pool_name="my-managed-pool",
         cron="0 1 * * *",
     )
