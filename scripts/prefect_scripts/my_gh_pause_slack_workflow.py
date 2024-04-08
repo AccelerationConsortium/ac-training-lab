c110c21b969a48bdd4a37fad1c6c6b4a36e5ee43
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 16:58:52 -0400

    Update my_gh_pause_slack_workflow.py

--- scripts/prefect_scripts/my_gh_pause_slack_workflow.py
+++ scripts/prefect_scripts/my_gh_pause_slack_workflow.py
@@ -2,16 +2,31 @@
 # https://prefecthq.github.io/prefect-slack/
 # https://github.com/PrefectHQ/interactive_workflow_examples
 
-from prefect import flow, get_run_logger, pause_flow_run
+from prefect import flow, get_run_logger, pause_flow_run, settings
 from prefect.blocks.notifications import SlackWebhook
+from prefect.context import get_run_context
 
-slack_block = SlackWebhook.load("help-us-humans")
+slack_block = SlackWebhook.load("prefect-test")
+
+MESSAGE = "This is an example of a human-in-the-loop flow using Prefect's interactive workflow features."  # noqa: E501
 
 
-@flow
-def greet_user():
+@flow(name="greet-slack-user")  # persist result or not?
+async def greet_user():
     logger = get_run_logger()
 
-    user = pause_flow_run(wait_for_input=str)
+    message = str(MESSAGE)  # modify as needed
+    flow_run = get_run_context().flow_run
+
+    if flow_run and settings.PREFECT_UI_URL:
+        flow_run_url = (
+            f"{settings.PREFECT_UI_URL.value()}/flow-runs/flow-run/{flow_run.id}"
+        )
+        message += f"\n\nOpen the <{flow_run_url}|paused flow run>, click 'Resume', and then submit your name."  # noqa: E501
+
+    await slack_block.notify(message)
+    user = await pause_flow_run(wait_for_input=str, timeout=60)
 
     logger.info(f"Hello, {user}!")
+
+    return user
