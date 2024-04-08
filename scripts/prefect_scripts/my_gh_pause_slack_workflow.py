a6c25d8b1b48003ea3b6cb445456bca06119d7ac
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 14:23:02 -0400

    change dir

--- scripts/prefect_scripts/my_gh_pause_slack_workflow.py
+++ scripts/prefect_scripts/my_gh_pause_slack_workflow.py
@@ -2,6 +2,16 @@
 # https://prefecthq.github.io/prefect-slack/
 # https://github.com/PrefectHQ/interactive_workflow_examples
 
+from prefect import flow, get_run_logger, pause_flow_run
 from prefect.blocks.notifications import SlackWebhook
 
 slack_block = SlackWebhook.load("help-us-humans")
+
+
+@flow
+def greet_user():
+    logger = get_run_logger()
+
+    user = pause_flow_run(wait_for_input=str)
+
+    logger.info(f"Hello, {user}!")
