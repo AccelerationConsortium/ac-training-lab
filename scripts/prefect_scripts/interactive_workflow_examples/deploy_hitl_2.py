feb971477ceee2e0695f267f8514fd772b2066a8
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 16:26:53 -0400

    Update deploy_hitl_2.py

--- scripts/prefect_scripts/interactive_workflow_examples/deploy_hitl_2.py
+++ scripts/prefect_scripts/interactive_workflow_examples/deploy_hitl_2.py
@@ -3,7 +3,10 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/AccelerationConsortium/ac-training-lab.git",
-        entrypoint="scripts/prefect_scripts/interactive_workflow_examples/human_in_the_loop_2.py:guessing_classifier",  # noqa: E501
+        entrypoint=(
+            "scripts/prefect_scripts/interactive_workflow_examples/"
+            "human_in_the_loop_2.py:classify_image"
+        ),
     ).deploy(name="human-in-the-loop", work_pool_name="my-managed-pool")
 
     1 + 1
