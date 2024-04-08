cb15ad37a4970b78e0fec1b57a1d8d07d8fb1189
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 14:23:58 -0400

    Update get_result.py

--- scripts/prefect_scripts/client_scripts/get_result.py
+++ scripts/prefect_scripts/client_scripts/get_result.py
@@ -14,10 +14,7 @@ async def get_result(flow_id):
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     result = loop.run_until_complete(get_result("bd33b4ee-7bf0-48e3-9629-23bdf121c107"))
-    # PersistedResult(type='reference', artifact_type=None,
-    # artifact_description=None, serializer_type='pickle',
-    # storage_block_id=UUID('1e4ce198-a25a-4808-81db-65cb30d0cffb'),
-    # storage_key='69b055e353b745249d493350b79e81e8')
+    # PersistedResult(type='reference', artifact_type=None, artifact_description=None, serializer_type='pickle', storage_block_id=UUID('1e4ce198-a25a-4808-81db-65cb30d0cffb'), storage_key='69b055e353b745249d493350b79e81e8') # noqa
     loop.close()
 
     1 + 1
