36a3f31a26d17862dee4013f689171a87b6af138
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-08 17:16:35 -0400

    Create create_suspend_slack_deployment.py

---
+++ scripts/prefect_scripts/create_suspend_slack_deployment.py
@@ -0,0 +1,11 @@
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/AccelerationConsortium/ac-training-lab.git",
+        entrypoint="scripts/prefect_scripts/my_gh_suspend_slack_workflow.py:greet_user",
+    ).deploy(
+        name="suspend-workflow-deployment",
+        work_pool_name="my-managed-pool",
+        cron="0 1 * * *",
+    )
