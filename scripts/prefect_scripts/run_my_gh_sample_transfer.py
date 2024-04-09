87641305f3e99fd83f03e51238fbb3ccaea53cd7
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-09 13:45:31 -0400

    Update run_my_gh_sample_transfer.py

--- scripts/prefect_scripts/run_my_gh_sample_transfer.py
+++ scripts/prefect_scripts/run_my_gh_sample_transfer.py
@@ -1,4 +1,6 @@
-from scripts.prefect_scripts.my_gh_sample_transfer_workflow import move_sample
+import asyncio
+
+from my_gh_sample_transfer_workflow import move_sample
 
 sample_link = "https://pioreactor.com/en-ca/collections/accessories-and-parts/products/20ml-glass-vial-closed-cap-and-stir-bar?variant=42836211761208"  # noqa: E501
 sample_name = "sample_100d793c"
@@ -7,11 +9,19 @@ source_name = "Automated capper/decapper"
 destination_link = "https://autotrickler.com/pages/autotrickler-v4"
 destination_name = "Autotrickler v4"
 
-move_sample(
-    sample_link,
-    sample_name,
-    source_link,
-    source_name,
-    destination_link,
-    destination_name,
-)
+
+async def main():
+    await move_sample(
+        sample_link,
+        sample_name,
+        source_link,
+        source_name,
+        destination_link,
+        destination_name,
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    1 + 1
