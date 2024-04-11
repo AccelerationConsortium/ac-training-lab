8c84da41dde1b37e0c30dd900b1b3d830a4b8174
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-04-11 10:24:30 -0400

    Update my_gh_sample_transfer_workflow.py

--- scripts/prefect_scripts/my_gh_sample_transfer_workflow.py
+++ scripts/prefect_scripts/my_gh_sample_transfer_workflow.py
@@ -2,6 +2,8 @@
 # https://prefecthq.github.io/prefect-slack/
 # https://github.com/PrefectHQ/interactive_workflow_examples
 
+import asyncio
+
 from prefect import flow, get_run_logger, pause_flow_run, settings
 from prefect.blocks.notifications import SlackWebhook
 from prefect.context import get_run_context
@@ -53,3 +55,25 @@ async def move_sample(
     logger.info(f"🚩 Flagged for review: {user_input.flag_for_review}")
 
     return user_input
+
+
+if __name__ == "__main__":
+    sample_link = "https://pioreactor.com/en-ca/collections/accessories-and-parts/products/20ml-glass-vial-closed-cap-and-stir-bar?variant=42836211761208"  # noqa: E501
+    sample_name = "sample_100d793c"
+    source_link = "https://ca.robotshop.com/products/dh-robotics-automated-screw-cap-decapper-intelligent-capping-machine"  # noqa: E501
+    source_name = "Automated capper/decapper"
+    destination_link = "https://autotrickler.com/pages/autotrickler-v4"
+    destination_name = "Autotrickler v4"
+
+    user_input = asyncio.run(
+        move_sample(
+            sample_link=sample_link,
+            sample_name=sample_name,
+            source_link=source_link,
+            source_name=source_name,
+            destination_link=destination_link,
+            destination_name=destination_name,
+        )  # type: ignore
+    )
+
+    1 + 1
