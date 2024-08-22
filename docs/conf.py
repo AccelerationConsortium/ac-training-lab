168a2399cc620c3429324d07d56a586fabf36f37
Sterling G. Baird <sterling.baird@utoronto.ca>
2024-08-21 23:32:55 -0400

    Add documentation for setting up new IoLT devices
    Fixes #5
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/AccelerationConsortium/ac-training-lab/issues/5?shareId=XXXX-XXXX-XXXX-XXXX).

--- docs/conf.py
+++ docs/conf.py
@@ -75,6 +75,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx_rtd_theme",
+    "sphinx_book_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -174,7 +175,7 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
