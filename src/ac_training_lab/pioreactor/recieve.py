13a44854c964c4c4e32b4d013fa54a7fa1c06b04
Enrui Lin <linedison3@gmail.com>
2024-10-18 17:02:52 -0400

    A short script for testing message receiving

---
+++ src/ac_training_lab/pioreactor/recieve.py
@@ -0,0 +1,48 @@
+import paho.mqtt.client as mqtt
+import lookhere
+# Define the MQTT settings
+# broker = lookhere.broker
+# port = lookhere.port
+# username = lookhere.username
+# password = lookhere.password
+
+"""
+A simple script that subscribes to a topic and prints the received messages.
+
+Useful for debugging and testing the MQTT broker.
+
+Author: Enrui (Edison) Lin
+"""
+
+broker = 'pio1.local'
+port = 1883
+username = 'pioreactor'
+password = 'raspberry'
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    client.subscribe("pioreactor/pio1/Edi/leds/intensity")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(f"Message received: {msg.payload.decode()} on topic {msg.topic}")
+
+    payload = msg.payload.decode()
+    if msg.topic == "pioreactor/pio1/Edi/temperature_automation":
+        print("\n")
+        print("Received temperature automation data")
+        # Do something with the payload
+        print(payload)
+        print(payload['target_temperature'])
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.username_pw_set(username, password)
+# client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS_CLIENT)
+client.connect(broker, port, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
+client.loop_forever()
