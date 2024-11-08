f4dd57c03c68e07046d206e869a96500b52be070
gursi26 <gursimar.batra@gmail.com>
2024-11-08 16:20:05 -0500

    ok, now i think its good

--- src/ac_training_lab/cobot280pi/demo.py
+++ src/ac_training_lab/cobot280pi/demo.py
@@ -2,8 +2,10 @@ from pymycobot.mycobot import MyCobot
 
 cobot = MyCobot("/dev/ttyAMA0", 1000000)
 
+
 def rise():
-	cobot.send_angles([0, 0, 0, 0, 0, 0], 100)
+    cobot.send_angles([0, 0, 0, 0, 0, 0], 100)
+
 
 def kill():
-	cobot.release_all_servos()
+    cobot.release_all_servos()
