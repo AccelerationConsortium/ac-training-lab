c0a55f2d20d9ad9210a71d340174fb065cc80339
gursi26 <gursimar.batra@gmail.com>
2024-11-08 16:16:16 -0500

    pls work

--- src/ac_training_lab/cobot280pi/utils.py
+++ src/ac_training_lab/cobot280pi/utils.py
@@ -1,26 +1,20 @@
 import logging
 import sys
 
+
 def setup_logger(logfile_name: str = "mqttcobot.log"):
-	logger = logging.getLogger('logger')
-	logger.setLevel(logging.INFO)
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.INFO)
 
-	console_handler = logging.StreamHandler(sys.stdout)
-	file_handler = logging.FileHandler(logfile_name)
+    console_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler(logfile_name)
 
-	formatter = logging.Formatter('[%(levelname)s - %(asctime)s]: %(message)s')
-	console_handler.setFormatter(formatter)
-	file_handler.setFormatter(formatter)
+    formatter = logging.Formatter("[%(levelname)s - %(asctime)s]: %(message)s")
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
-	if not logger.hasHandlers():
-		logger.addHandler(console_handler)
-		logger.addHandler(file_handler)
-	logger.propagate = False
-	return logger
-
-
-def truncate_string(s, max_length = 50):
-    if len(s) <= max_length:
-        return s
-    half = (max_length - 3) // 2
-    return f"{s[:half]}...{s[-half:]}"
+    if not logger.hasHandlers():
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+    logger.propagate = False
+    return logger
