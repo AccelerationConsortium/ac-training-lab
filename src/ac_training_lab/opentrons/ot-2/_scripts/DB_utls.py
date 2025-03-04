8643491919215703ad4880a4bc006ced1ccc957f
Neil-YL <yanghuang.liu92@gmail.com>
2025-03-04 10:54:39 -0500

    Update OT-2 MQTT script and DB utils for well status and students

---
+++ src/ac_training_lab/opentrons/ot-2/_scripts/DB_utls.py
@@ -0,0 +1,151 @@
+from pymongo import MongoClient
+from datetime import datetime
+from prefect import task
+import pandas as pd
+import os
+
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+
+blinded_connection_string = os.getenv("blinded_connection_string")
+
+connection_string = blinded_connection_string.replace("<db_password>", MONGODB_PASSWORD)
+
+
+@task
+def generate_empty_well():
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]  
+    collection = db["wells"] 
+    rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    columns = [str(i) for i in range(1, 13)]
+    for row in rows:
+        for col in columns:
+            well = f"{row}{col}"
+            metadata = {
+                "well": well,
+                "status": "empty",
+                "project": "OT2"
+            }
+
+            #send_data_to_mongodb(collection="wells", data=metadata)
+            query = {"well": well}
+            update_data = {"$set": metadata}  
+            result = collection.update_one(query, update_data, upsert=True)
+            
+    # close connection
+    dbclient.close()
+
+@task 
+def update_used_wells(used_wells):
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]  
+    collection = db["wells"] 
+
+    for well in used_wells:
+        metadata = {
+            "well": well,
+            "status": "used",
+            "project": "OT2"
+        }
+        #send_data_to_mongodb(collection="wells", data=metadata)
+        query = {"well": well}
+        update_data = {"$set": metadata}  
+        result = collection.update_one(query, update_data, upsert=True)
+        
+    # close connection
+    dbclient.close()
+
+@task
+def find_unused_wells():
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]  
+    collection = db["wells"] 
+    query = {"status": "empty"}
+    response = list(collection.find(query))  
+    df = pd.DataFrame(response)
+
+    # Extract the "well" column as a list
+    if "well" not in df.columns:
+        raise ValueError("No available wells.")
+    # Sort obtained list
+    def well_sort_key(well):
+        row = well[0]  # A, B, C, ...
+        col = int(well[1:])  # 1, 2, ..., 12
+        return (row, col)
+
+    empty_wells = sorted(df["well"].tolist(), key=well_sort_key)
+    #print(empty_wells)
+    
+    # close connection
+    dbclient.close()
+
+    # Check if there are any empty wells
+    if len(empty_wells) == 0:
+        raise ValueError("No empty wells found")
+    #print(empty_wells)
+    return empty_wells
+    
+@task 
+def save_result(result_data):
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]  
+    collection = db["MSE403_result"] 
+    #collection = db["test_result"] 
+    result_data["timestamp"] = datetime.utcnow()  # UTC time
+    insert_result = collection.insert_one(result_data)
+    inserted_id = insert_result.inserted_id
+    # close connection
+    dbclient.close()
+    return inserted_id    
+
+def get_student_quota(student_id):
+    with MongoClient(connection_string) as client:
+        db = client["LCM-OT-2-SLD"]  
+        collection = db["student"] 
+        student = collection.find_one({"student_id": student_id})
+        if student is None:
+            raise ValueError(f"Student ID '{student_id}' not found in the database.")
+        return student.get("quota", 0) 
+  
+def decrement_student_quota(student_id):
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]  
+    collection = db["student"] 
+
+    student = collection.find_one({"student_id": student_id})
+    if not student:
+        return f"Student ID {student_id} not found."
+    if student.get("quota", 0) <= 0:
+        return f"Student ID {student_id} has no remaining quota."
+
+    
+    result = collection.update_one(
+            {"student_id": student_id, "quota": {"$gt": 0}},  
+            {"$inc": {"quota": -1}} 
+        )
+
+    if result.modified_count > 0:
+        return f"Student ID {student_id}'s quota update successfully."
+    else:
+        return f"Quota update failed for Student ID {student_id}."
+    
+def add_student_quota(student_id, quota):
+    """
+    Adds a new student with a given quota.
+    :param student_id: The ID of the student.
+    :param quota: The initial quota for the student.
+    """
+    dbclient = MongoClient(connection_string)
+    db = dbclient["LCM-OT-2-SLD"]
+    collection = db["student"]
+    student_data = {"student_id": student_id, "quota": quota}
+    collection.update_one({"student_id": student_id}, {"$set": student_data}, upsert=True)
+    dbclient.close()
+    
+
+if __name__ == "__main__":
+    generate_empty_well()
+    #find_unused_wells()
+    #test_id = "test"
+    #quota = 999
+    #add_student_quota(test_id, quota)
--- src/ac_training_lab/opentrons/ot-2/_scripts/OT2mqtt.py
+++ src/ac_training_lab/opentrons/ot-2/_scripts/OT2mqtt.py
@@ -1,11 +1,11 @@
 import json
-from queue import Empty, Queue
-from time import sleep
-
-import opentrons.execute
+from queue import Queue, Empty
+from time import sleep,time
 import paho.mqtt.client as mqtt
+import opentrons.execute
 
-protocol = opentrons.execute.get_protocol_api("2.16")
+
+protocol = opentrons.execute.get_protocol_api('2.16')
 
 OT2_SERIAL = "OT2CEP20240218R04"
 PICO_ID = "e66130100f895134"
@@ -18,8 +18,8 @@ port = 8883
 
 OT2_COMMAND_TOPIC = f"command/ot2/{OT2_SERIAL}/pipette"
 OT2_STATUS_TOPIC = f"status/ot2/{OT2_SERIAL}/complete"
-# SENSOR_COMMAND_TOPIC = f"command/picow/{PICO_ID}/as7341/read"
-# SENSOR_DATA_TOPIC = f"color-mixing/picow/{PICO_ID}/as7341"
+#SENSOR_COMMAND_TOPIC = f"command/picow/{PICO_ID}/as7341/read" 
+#SENSOR_DATA_TOPIC = f"color-mixing/picow/{PICO_ID}/as7341"
 
 # Initialize MQTT client and sensor data queue
 client = mqtt.Client()
@@ -27,8 +27,6 @@ client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS_CLIENT)  # type: ignore
 client.username_pw_set(username, password)
 
 command_queue = Queue()
-
-
 # MQTT Callbacks
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT Broker with result code", rc)
@@ -36,17 +34,16 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    payload = msg.payload.decode("utf-8")
+    payload = msg.payload.decode("utf-8")  
     print(f"Received message on topic {msg.topic}: {payload}")
     try:
-        payload = json.loads(payload)
+        payload = json.loads(payload) 
         if msg.topic == OT2_COMMAND_TOPIC:
-            command_queue.put(payload)
+            command_queue.put(payload) 
 
     except json.JSONDecodeError as e:
         print(f"Failed to decode JSON payload: {e}")
-
-
+        
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(host, port)
@@ -54,32 +51,38 @@ client.loop_start()
 
 print("MQTT client connected")
 
-protocol.home()  # home to know MQTT cilent is connected
-# Define labware and pipettes
+protocol.home() #home to know MQTT cilent is connected
+# Define labware and pipettes 
 
-# changed file path before nohup run
-with open("ac_color_sensor_charging_port.json") as labware_file1:
+#changed file path before nohup run
+#load wireless charging port
+with open('/var/lib/jupyter/notebooks/ac_color_sensor_charging_port.json') as labware_file1:
     labware_def1 = json.load(labware_file1)
     tiprack_2 = protocol.load_labware_from_definition(labware_def1, 10)
-
-with open("ac_6_tuberack_15000ul.json") as labware_file2:
+#load 3x2 vials rack    
+with open('/var/lib/jupyter/notebooks/ac_6_tuberack_15000ul.json') as labware_file2:
     labware_def2 = json.load(labware_file2)
-    reservoir = protocol.load_labware_from_definition(labware_def2, 3)
+    reservoir = protocol.load_labware_from_definition(labware_def2, 3)    
+#load other labwares from Opentrons's labware library
+plate = protocol.load_labware(
+    load_name="corning_96_wellplate_360ul_flat",
+    location=1)
 
-plate = protocol.load_labware(load_name="corning_96_wellplate_360ul_flat", location=1)
-
-tiprack_1 = protocol.load_labware(load_name="opentrons_96_tiprack_300ul", location=9)
+tiprack_1 = protocol.load_labware(
+        load_name="opentrons_96_tiprack_300ul",
+        location=9)
 
 p300 = protocol.load_instrument(
-    instrument_name="p300_single_gen2", mount="right", tip_racks=[tiprack_1]
-)
+        instrument_name="p300_single_gen2",
+        mount="right",
+        tip_racks=[tiprack_1])
 
-p300.well_bottom_clearance.dispense = 8
+p300.well_bottom_clearance.dispense=8
 
 
 print("Labwares loaded")
 
-
+#this includes a 3-color mix and a measurement 
 def mix_color(payload):
     R = payload["command"]["R"]
     Y = payload["command"]["Y"]
@@ -88,97 +91,89 @@ def mix_color(payload):
     session_id = payload["session_id"]
     experiment_id = payload["experiment_id"]
 
-    total = int(R + Y + B)
-    if total <= 300:
-        raise ValueError("The sum of the proportions must be smaller than 300")
+    total = R+Y+B
+    if total > 300:
+        raise ValueError("The sum of the proportions must be not greater than 300")
 
-    position = ["B1", "B2", "B3"]
+    position = ["B1", "B2", "B3"]  # R,Y,B paint vial on the position of the 3x2 vials rack
     portion = {"B1": R, "B2": Y, "B3": B}
-    # total_volume = 280
-    red_volume = R  # int(portion["B1"] * total_volume)
-    yellow_volume = Y  # int(portion["B2"] * total_volume)
-    blue_volume = B  # int(portion["B3"] * total_volume)
+    #total_volume = 280
+    red_volume = R #int(portion["B1"] * total_volume)
+    yellow_volume = Y # int(portion["B2"] * total_volume)
+    blue_volume = B #int(portion["B3"] * total_volume)
     color_volume = {"B1": red_volume, "B2": yellow_volume, "B3": blue_volume}
-
-    # To DO: start the mixing from the color with the smallest volume
+    
 
     for pos in position:
-        if float(portion[pos]) != 0.0:
+        if float(portion[pos]) != 0.0:    #if zero, skip this color aspiration process
             p300.pick_up_tip(tiprack_1[pos])
-            p300.aspirate(color_volume[pos], reservoir[pos])
-            p300.dispense(color_volume[pos], plate[mix_well])
-            p300.default_speed = 100  # reduce pipette speed
+            p300.aspirate(color_volume[pos],reservoir[pos])
+            p300.dispense(color_volume[pos],plate[mix_well])
+            p300.default_speed = 100         # reduce pipette speed 
             p300.blow_out(reservoir["A1"].top(z=-5))
-            p300.default_speed = 400  # reset pipette speed
+            p300.default_speed = 400         # reset pipette speed
             p300.drop_tip(tiprack_1[pos])
-
+    
     p300.pick_up_tip(tiprack_2["A2"])
-    p300.move_to(plate[mix_well].top(z=3))
-
-    # payloadtosent = json.dumps(payload)
-    # print("Sending read command to sensor...")
-    # client.publish(SENSOR_COMMAND_TOPIC, payloadtosent, qos=2)
-
+    p300.move_to(plate[mix_well].top(z=-1.3))
+    
+    
+    #payloadtosent = json.dumps(payload)  
+   # print("Sending read command to sensor...")
+   # client.publish(SENSOR_COMMAND_TOPIC, payloadtosent, qos=2) # send a read commmand to sensor
+   
     print("Sending status to HF...")
-
-    payload = (
-        f'{{"status": {{"sensor_status":"in_place"}}, '
-        f'"experiment_id": "{experiment_id}", '
-        f'"session_id": "{session_id}"}}'
-    )
-
-    client.publish(OT2_STATUS_TOPIC, payload, qos=2)  # send a status message back to HF
-
+    payload = f'{{"status": {{"sensor_status":"in_place"}}, "experiment_id": "{experiment_id}", "session_id": "{session_id}"}}'    
+    client.publish(OT2_STATUS_TOPIC, payload, qos=2) # send a status message back to HF
+   
+    
 
 def move_sensor_back(payload):
     results_status = payload["command"]["sensor_status"]
     session_id = payload["session_id"]
     experiment_id = payload["experiment_id"]
-
+    
     p300.drop_tip(tiprack_2["A2"].top(z=-80))
-    # protocol.home()
+    #protocol.home()
 
-    payload = (
-        f'{{"status": {{"sensor_status":"charging"}}, '
-        f'"experiment_id": "{experiment_id}", '
-        f'"session_id": "{session_id}"}}'
-    )
-    client.publish(OT2_STATUS_TOPIC, payload, qos=2)  # send a status message back to HF
+    payload = f'{{"status": {{"sensor_status":"charging"}}, "experiment_id": "{experiment_id}", "session_id": "{session_id}"}}'
+    client.publish(OT2_STATUS_TOPIC, payload, qos=2) # send a status message back to HF
 
     if results_status == "sensor_timeout":
         protocol.home()
 
-
 def handle_command(payload):
-
+    
     if {"R", "Y", "B", "well"}.issubset(payload["command"].keys()):
         print(f"Handling mix command: {payload}")
         mix_color(payload)
-
+        
+    
     elif {"sensor_status"}.issubset(payload["command"].keys()):
-        print("Sensor measure complete")
+        print(f"Sensor measure complete")
         move_sensor_back(payload)
+        
+        
+print("OT-2 is waiting for command")  
+protocol.home() #home to know OT-2 is ready
 
-
-print("OT-2 is waiting for command")
-protocol.home()  # home to know OT-2 is ready
-
-
+ 
 # Keep protocol active
 while True:
     try:
         command = command_queue.get(timeout=1)
         print(f"Processing command from queue: {command}")
-
+        
         if "command" in command and "experiment_id" in command:
             try:
                 handle_command(command)
             except Exception as e:
                 print(f"Error processing command: {e}")
     except Empty:
-
-        print("No commands in queue. Waiting...")
+        
+        pass
+    
     except Exception as e:
-
+      
         print(f"Unexpected error in main loop: {e}")
     sleep(1)
