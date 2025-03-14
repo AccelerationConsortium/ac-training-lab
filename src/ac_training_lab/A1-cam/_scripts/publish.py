572f2f98a44be69a443fd4e508b56d240e2a1d94
Sterling G. Baird <sterling.baird@utoronto.ca>
2025-03-14 17:44:27 -0400

    refactor MQTT client setup and enhance message handling for sensor data

--- src/ac_training_lab/A1-cam/_scripts/publish.py
+++ src/ac_training_lab/A1-cam/_scripts/publish.py
@@ -1,26 +1,66 @@
-#!/usr/bin/env python3
-import json
 import paho.mqtt.client as mqtt
+import json
+from queue import Queue
+
 from my_secrets import (
     CAMERA_READ_TOPIC,
-    MQTT_HOST,
-    MQTT_PORT,
+    CAMERA_WRITE_TOPIC,
     MQTT_USERNAME,
     MQTT_PASSWORD,
+    MQTT_HOST,
 )
 
-# Initialize the MQTT client for the orchestrator
-client = mqtt.Client()
-client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+sensor_data_queue: "Queue[dict]" = Queue()
 
-client.connect(MQTT_HOST, MQTT_PORT, 60)
 
-# Publish a command message to trigger image capture
-command_payload = json.dumps({"command": "capture_image"})
+def get_paho_client(
+    sensor_data_topic, hostname, username, password=None, port=8883, tls=True
+):
 
-client.publish(CAMERA_READ_TOPIC, command_payload, qos=2)
-print("Published capture command to", CAMERA_READ_TOPIC)
+    client = mqtt.Client(
+        mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv5
+    )  # create new instance
 
-# Disconnect the client after publishing
-client.disconnect()
+    def on_message(client, userdata, msg):
+        sensor_data_queue.put(json.loads(msg.payload))
+
+    # The callback for when the client receives a CONNACK response from the server.
+    def on_connect(client, userdata, flags, rc, properties=None):
+        if rc != 0:
+            print("Connected with result code " + str(rc))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe(sensor_data_topic, qos=1)
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    # enable TLS for secure connection
+    if tls:
+        client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS_CLIENT)
+    # set username and password
+    client.username_pw_set(username, password)
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client.connect(hostname, port)
+    client.subscribe(sensor_data_topic, qos=2)
+
+    return client
+
+
+def send_and_receive(client, command_topic, msg, queue_timeout=60):
+    client.publish(command_topic, msg, qos=2)
+
+    client.loop_start()
+
+    while True:
+        sensor_data = sensor_data_queue.get(True, queue_timeout)
+        client.loop_stop()
+        return sensor_data
+
+
+client = get_paho_client(
+    CAMERA_WRITE_TOPIC, MQTT_HOST, MQTT_USERNAME, password=MQTT_PASSWORD
+)
+
+onboard_temp = send_and_receive(client, CAMERA_READ_TOPIC, "toggle", queue_timeout=30)
+print(onboard_temp)
