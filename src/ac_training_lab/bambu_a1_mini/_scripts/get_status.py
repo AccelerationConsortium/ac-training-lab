f2cacd171e3c3a54ef1f2bf5305ccf69308a8454
Sterling Baird <sterling.baird@utoronto.ca>
2025-03-20 16:40:19 -0400

    mv manual_print and create get_status mwe, also website is useful

---
+++ src/ac_training_lab/bambu_a1_mini/_scripts/get_status.py
@@ -0,0 +1,24 @@
+# https://mchrisgm.github.io/bambulabs_api/examples.html
+
+from time import sleep
+import bambulabs_api as bl
+
+from my_secrets import IP, SERIAL, ACCESS_CODE
+
+printer = bl.Printer(IP, ACCESS_CODE, SERIAL)
+
+printer.connect()
+
+sleep(2.0)
+
+status = printer.get_state()
+print(f"Printer status: {status}")
+
+bed_temperature = printer.get_bed_temperature()
+nozzle_temperature = printer.get_nozzle_temperature()
+speed = printer.get_print_speed()
+print(
+    f"Printer status: {status}, Bed temp: {bed_temperature}, "
+    f"Nozzle temp: {nozzle_temperature},"
+    f"Print speed: {speed}"
+)
