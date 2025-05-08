b84af7174372db572b4b5bff71f676e8a1b23377
Sterling G. Baird <sterling.baird@utoronto.ca>
2025-05-08 17:08:25 -0400

    PRIVACY_STATUS as var

--- src/ac_training_lab/picam/my_secrets_example.py
+++ src/ac_training_lab/picam/my_secrets_example.py
@@ -2,3 +2,4 @@ STREAM_URL = "your_stream_url_here"  # e.g., rtmp://a.rtmp.youtube.com/live2
 STREAM_KEY = "your_stream_key_here"
 LAMBDA_FUNCTION_URL = "your_Lambda_function_url"
 DEVICE_NAME = "device name for YTB stream key"
+PRIVACY_STATUS = "private"  # or "public" or "unlisted"
