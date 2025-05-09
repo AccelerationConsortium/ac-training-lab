f764930650da7a67c4274a85fab20bc74816961f
Sterling G. Baird <sterling.baird@utoronto.ca>
2025-05-09 16:32:31 -0400

    camera flip toggle

--- src/ac_training_lab/picam/my_secrets_example.py
+++ src/ac_training_lab/picam/my_secrets_example.py
@@ -3,3 +3,9 @@ STREAM_KEY = "your_stream_key_here"
 LAMBDA_FUNCTION_URL = "your_Lambda_function_url"
 DEVICE_NAME = "device name for YTB stream key"
 PRIVACY_STATUS = "private"  # or "public" or "unlisted"
+
+# Set to true if using default orientation as shown in
+# https://www.pishop.ca/product/pro-mini-camera-mount-for-raspberry-pi-zero/.
+# Otherwise, if you're using a setup with a short cable and the 3D printed part,
+# set to False (which will flip both vertically and horizontally)
+CAMERA_FLIP = True
