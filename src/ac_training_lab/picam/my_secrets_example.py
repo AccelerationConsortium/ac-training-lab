cd19c5d963796ff6255137e1af9de0a80a0e15c5
Neil-YL <yanghuang.liu92@gmail.com>
2025-05-10 22:15:04 -0400

    update device.py for cam_name workflow_name

--- src/ac_training_lab/picam/my_secrets_example.py
+++ src/ac_training_lab/picam/my_secrets_example.py
@@ -1,7 +1,6 @@
-STREAM_URL = "your_stream_url_here"  # e.g., rtmp://a.rtmp.youtube.com/live2
-STREAM_KEY = "your_stream_key_here"
 LAMBDA_FUNCTION_URL = "your_Lambda_function_url"
-DEVICE_NAME = "device name for YTB stream key"
+CAM_NAME = "your_camera_name"
+WORKFLOW_NAME = "your_workflow_name"
 PRIVACY_STATUS = "private"  # "private", "public", or "unlisted"
 
 # Set to true if using default orientation as shown in
